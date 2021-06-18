38e5affadc15d50b084cf731eb200a44ba290101
tanmay <tanmaykachroo@gmail.com>
2021-06-18 18:41:27 +0530

    ok

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,18 +5,33 @@ import { HiOutlineMail } from "react-icons/hi";
 const SocialFlex = () => {
   return (
     <div className="social-flex">
-      <a href ='#' className="email">
+      <a href="#" className="email">
         <HiOutlineMail /> &nbsp;Email: tanmaykachroo@gmail.com
       </a>
-      <a href="https://github.com/tanmayk5" target="_blank" className="social">
+      <a
+        href="https://github.com/tanmayk5"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <GrGithub />
         &nbsp;Github
       </a>
-      <a href="https://www.linkedin.com/" target="_blank" className="social">
+      <a
+        href="https://www.linkedin.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <FaLinkedinIn />
         &nbsp;Linkedin
       </a>
-      <a href="https://www.twitter.com/" target="_blank" className="social">
+      <a
+        href="https://www.twitter.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <RiTwitterFill />
         &nbsp;Twitter
       </a>
