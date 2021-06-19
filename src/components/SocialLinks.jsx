3a2ad5b4b78747ea75b95d8f42a1493d44caa7d3
tanmay <tanmaykachroo@gmail.com>
2021-06-19 14:13:53 +0530

    ok

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,12 +2,27 @@ import { RiTwitterFill } from "react-icons/ri";
 import { GrGithub } from "react-icons/gr";
 import { FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
+import { BiClipboard } from "react-icons/bi";
 const SocialFlex = () => {
+  const copyEmail=()=>{
+    navigator.clipboard.writeText('tanmaykachroo@gmail.com');
+    const toolText = document.querySelector('.copy-tooltext')
+    toolText.textContent ='Email Copied!'
+    setTimeout(()=>{
+    toolText.textContent = "Copy Email";
+
+    },2000)
+  }
   return (
     <div className="social-flex">
-      <a href="/" className="email">
-        <HiOutlineMail /> &nbsp;Email: tanmaykachroo@gmail.com
-      </a>
+      <div>
+        <div className="email">
+          <HiOutlineMail /> &nbsp;Email: tanmaykachroo@gmail.com
+        </div>
+        <span class="copy" onClick={copyEmail}>
+          <BiClipboard /> <span className="copy-tooltext">Copy Email</span>
+        </span>
+      </div>
       <a
         href="https://github.com/tanmayk5"
         target="_blank"
