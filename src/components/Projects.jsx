aaf5ceae1a50f6916838d00a8dfaea9aebeaeb40
tanmay <tanmaykachroo@gmail.com>
2021-06-19 16:51:40 +0530

    ok

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,15 @@
 import projects from "../projects";
 import Project from "./Project";
-
+import { GrReactjs, GrNode } from "react-icons/gr";
+import { SiFirebase, SiHtml5 } from "react-icons/si";
 const Projects = () => {
   return (
     <>
       <div className="page-heading">Projects</div>
+      <div className="projects-icons">
+        <GrReactjs className='react-icon'/>
+        <GrNode /> <SiFirebase /> <SiHtml5 />
+      </div>
       <div className="projects-grid projects-page-grid">
         {projects.map((project) => {
           return (
